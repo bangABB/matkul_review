e832447b8e25552fd305d7735fb3e5141764b9c2
bil <118203904+bangABB@users.noreply.github.com>
2023-09-13 01:03:02 +0700

    implemented the data type structure in views

--- main/views.py
+++ main/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
-
 def show_main(request):
-    context = {
-        'name': 'Kelas SDA',
-        'class': 'Kelas A ganteng',
-        'jumlah':'101',
-        'deskripsi':'matkul paling susah sejagat raya, tungguin abis uts deh',
-        'difficulty':'halooo cuma 11/10 kok',
-        'nilai' : 'B-'
+    
+    context = [
+        {
+            'name': 'SDA',
+            'class': 'A',
+            'amount': '101',
+            'deskripsi': 'matkul paling susah sejagat raya, tungguin abis uts deh',
+            'nilai': 'B-',
+        },
+        {
+            'name': 'Statprob',
+            'class': 'B',
+            'amount': '70',
+            'deskripsi': 'statprob keren',
+            'nilai': 'C',
+        },
+    ]
+
+    user_data = {
+        'nama_user': 'Masabil Arraya Muhammad',
+        'kelas_user': 'PBP A',
+        'NPM' : "2206082101"
     }
 
-    return render(request, "main.html", context)
-# Create your views here.
+    apllication_data = {
+        'nama_aplikasi':'REVIEW MATKUL',
+        'tanggal_dibuat':'13/09/2023'
+    }
+
+    return render(request, "main.html", {"view": context, "user_data": user_data, "application_data":apllication_data})
