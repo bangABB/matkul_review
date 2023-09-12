fff6e9ad4f2c7e4380c39da8f910da6a1adb18ef
bil <118203904+bangABB@users.noreply.github.com>
2023-09-12 12:27:07 +0700

    Added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    date_added = models.DateField(auto_now_add=True)
+    classes = models.CharField(max_length=1)
+    jumlah = models.IntegerField()
+    description = models.TextField()
+    deskripsi = models.TextField()
+    difficulty = models.TextField()
+    nilai = models.TextField(max_length=2)
