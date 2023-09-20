c62e4e920eb46f9ba7a90db7586ac3581ed022ba
bil <118203904+bangABB@users.noreply.github.com>
2023-09-20 08:38:02 +0700

    Aman ENV

--- matkul_review/settings.py
+++ matkul_review/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import env # Tambahkan kode berikut
+import environ # Tambahkan kode berikut
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = env.Env()
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
